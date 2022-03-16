f02f626ae1d2a26b6c16732b01d812fece3beb1d
Ahmadfauzi1203 <ahmadfauzi12346@gmail.com>
2022-03-16 14:06:49 +0700

    menambahkan 1 file

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+    class Contoh1 extends CI_Controller
+    {
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama saya Ahmad Fauzi
+            Saya sementara tinggal di daerah Cimanggis
+            olahraga yang saya sukai adalah Panahan";
+        }
+    }
+?>
